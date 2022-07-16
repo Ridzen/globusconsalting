04d0335bc9dc83612fe2e59d20f2a6d1108c95cb
Ridzen <ridze00@gmail.com>
2022-07-16 07:10:08 -0500

    changed requiremnts

--- globusconsalting/wsgi.py
+++ globusconsalting/wsgi.py
@@ -10,7 +10,8 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
+from globusconsalting.configurations import config
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'globusconsalting.settings')
 
+config()
 application = get_wsgi_application()
