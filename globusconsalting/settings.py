8f22a359c6acceeac057bd3ff4058eac8aefad1b
Ridzen <ridze00@gmail.com>
2022-07-15 22:34:38 -0500

    init

--- globusconsalting/settings.py
+++ globusconsalting/settings.py
@@ -60,9 +60,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'globusconsalting.urls'
+
 
 TEMPLATES = [
     {
